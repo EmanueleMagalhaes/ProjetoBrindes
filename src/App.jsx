d98043fa03e7df21f16c0bc8123912f69eef9e03
Emanuele Magalhães <130506951+EmanueleMagalhaes@users.noreply.github.com>
2025-09-02 19:46:27 -0300

    Implementação da tela cadastro de produtos

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 
 import EnvioForm from "./pages/Envios/EnvioForm/EnvioForm";
 import EnvioList from "./pages/Envios/EnviosList/EnviosList";
+import CadastroProdutos from "./pages/Produtos/CadastroProdutos";
 
 function App() {
   return (
     <Routes>
       <Route path="/" Component={EnvioForm} />
       <Route path="/envios" Component={EnvioList} />
+      <Route path="/produtos/cadastro" Component={CadastroProdutos} />
     </Routes>
   );
 }
