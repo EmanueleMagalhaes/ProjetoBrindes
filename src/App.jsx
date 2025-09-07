00883e0e688ded671acc63314bed87b806a8dc9e
Emanuele Magalhães <130506951+EmanueleMagalhaes@users.noreply.github.com>
2025-09-07 16:38:59 -0300

    ajuste do caminho lista produtos

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import EnvioForm from "./pages/Envios/EnvioForm/EnvioForm";
 import EnvioList from "./pages/Envios/EnviosList/EnviosList";
 import CadastroProdutos from "./pages/Produtos/CadastroProdutos";
-import ListaProdutos from "./pages/Produtos/CadastroProdutos";
+import ListaProdutos from "./pages/Produtos/ProdutoList";
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
       <Route path="/envios/novo" Component={EnvioForm} />
       <Route path="/envios/editar/:id" Component={EnvioForm} />
       <Route path="/produtos/cadastro" Component={CadastroProdutos} />
-      <Route path="/produtos/editar/:id" Component={ListaProdutos} />
+      <Route path="/produtos/lista" Component={ListaProdutos} />
     </Routes>
   );
 }
